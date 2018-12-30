db96432b23898cda348850e45d9665b8167ef43b
Dylan Michael Labat <dml252@live.com>
2018-12-30 22:05:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,9 +67,6 @@ def runner
   welcome
   card_total = initial_round
   display_card_total(card_total)
-  until card_total > 21 || answer == "s"
-    hit?(card_total)
-  end
   end_game(card_total)
 end
     
